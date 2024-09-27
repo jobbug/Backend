a19328f0055f6e8f4c44b623b89fe891a5b12efc
oosedus <seayeon0729@gmail.com>
2024-09-27 13:02:11 +0900

    [init] : 프로젝트 생성

---
+++ src/main/java/com/example/jobbug/JobbugApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobbug/JobbugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
