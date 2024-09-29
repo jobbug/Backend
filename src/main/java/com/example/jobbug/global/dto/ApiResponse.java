aa10edcff7969b83bd2a15288e70cdbdb812b6cc
oosedus <seayeon0729@gmail.com>
2024-09-29 10:26:56 +0900

    [feat] global response 설정

---
+++ src/main/java/com/example/jobbug/global/dto/ApiResponse.java
@@ -0,0 +1,5 @@
+package com.example.jobbug.global.dto;
+
+public class ApiResponse {
+
+}
---
+++ src/main/java/com/example/jobbug/global/dto/SuccessNonDataResponse.java
@@ -0,0 +1,19 @@
+package com.example.jobbug.global.dto;
+
+import com.example.jobbug.global.exception.enums.SuccessCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SuccessNonDataResponse extends ApiResponse{
+    private final int code;
+    private final String message;
+
+    public static SuccessNonDataResponse success(SuccessCode successCode) {
+        return new SuccessNonDataResponse(successCode.getHttpStatus().value(), successCode.getMessage());
+    }
+}
