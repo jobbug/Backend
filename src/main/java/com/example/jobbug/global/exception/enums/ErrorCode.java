4814543f6cf0f1d3b61a7c9ecd91da8f40faea42
oosedus <seayeon0729@gmail.com>
2024-10-02 02:10:56 +0900

    [feat] : ErrorCode 추가

--- src/main/java/com/example/jobbug/global/exception/enums/ErrorCode.java
+++ src/main/java/com/example/jobbug/global/exception/enums/ErrorCode.java
@@ -22,6 +22,8 @@ public enum ErrorCode {
 
     //404
     NOT_FOUND_RESOURCE_EXCEPTION(HttpStatus.NOT_FOUND, "E404", "해당 자원을 찾을 수 없습니다."),
+    NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "E000", "해당 유저를 찾을 수 없습니다."),
+    NOT_FOUND_ADDRESS_EXCEPTION(HttpStatus.NOT_FOUND, "E301", "해당 주소를 찾을 수 없습니다."),
 
     // 405 METHOD_NOT_ALLOWED
     METHOD_NOT_ALLOWED_EXCEPTION(HttpStatus.METHOD_NOT_ALLOWED, "E405", "지원하지 않는 메소드 입니다."),
